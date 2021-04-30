bf0d79445d132fe6025aee475a9b5a45cac8fe6f
Jacob Faulk <jacob5567@gmail.com>
2021-04-30 02:18:53 -0700

    Finish first 2 chunks and level generator

---
+++ Assets/Scripts/LinearLevelGenerator.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearLevelGenerator : MonoBehaviour
+{
+
+    public GameObject start;
+    public GameObject player;
+    public GameObject[] prefabs;
+    private GameObject[] currentlyGenerated;
+    private float currentLocation;
+    private float currentElevation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLocation = start.gameObject.GetComponent<StartingZoneInfo>().length;
+        currentElevation = start.gameObject.GetComponent<StartingZoneInfo>().elevationChange;
+
+        generateNewChunk();
+        generateNewChunk();
+        generateNewChunk();
+        generateNewChunk();
+        generateNewChunk();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void generateNewChunk()
+    {
+        int index = Random.Range(0, prefabs.Length);
+        Debug.Log(index);
+        Instantiate(prefabs[index], new Vector3(currentLocation, currentElevation, 0), Quaternion.identity);
+        currentLocation += prefabs[index].gameObject.GetComponent<PieceInfo>().length;
+        currentElevation += prefabs[index].gameObject.GetComponent<PieceInfo>().elevationChange;
+    }
+}
