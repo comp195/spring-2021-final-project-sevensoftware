533dc9e6620e4ecf581efc4feef9eaff99461b04
Jacob Faulk <jacob5567@gmail.com>
2021-04-30 15:17:50 -0700

    Complete dynamic level generation

--- Assets/Scripts/LinearLevelGenerator.cs
+++ Assets/Scripts/LinearLevelGenerator.cs
@@ -8,31 +8,48 @@ public class LinearLevelGenerator : MonoBehaviour
     public GameObject start;
     public GameObject player;
     public GameObject[] prefabs;
-    private GameObject[] currentlyGenerated;
+    private Queue<GameObject> currentlyGenerated;
     private float currentLocation;
     private float currentElevation;
+    public const int NUM_GENERATED_CHUNKS = 3;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentLocation = start.gameObject.GetComponent<StartingZoneInfo>().length;
-        currentElevation = start.gameObject.GetComponent<StartingZoneInfo>().elevationChange;
+        currentlyGenerated = new Queue<GameObject>();
+        currentlyGenerated.Enqueue(start);
 
-        for (int i = 0; i < 10; i++)
+        currentLocation = start.gameObject.GetComponent<PieceInfo>().length;
+        currentElevation = start.gameObject.GetComponent<PieceInfo>().elevationChange;
+
+        for (int i = 0; i < NUM_GENERATED_CHUNKS * 2; i++)
             generateNewChunk();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        int i = 0;
+        float xDistance = currentlyGenerated.Peek().transform.position.x;
+        foreach (GameObject o in currentlyGenerated)
+        {
+            xDistance += o.gameObject.GetComponent<PieceInfo>().length;
+            i++;
+            if (i >= NUM_GENERATED_CHUNKS)
+                break;
+        }
+        if (player.transform.position.x > xDistance)
+        {
+            Destroy(currentlyGenerated.Dequeue());
+            generateNewChunk();
+        }
     }
 
     private void generateNewChunk()
     {
         int index = Random.Range(0, prefabs.Length);
         Debug.Log(index);
-        Instantiate(prefabs[index], new Vector3(currentLocation, currentElevation, 0), Quaternion.identity);
+        currentlyGenerated.Enqueue(Instantiate(prefabs[index], new Vector3(currentLocation, currentElevation, 0), Quaternion.identity));
         currentLocation += prefabs[index].gameObject.GetComponent<PieceInfo>().length;
         currentElevation += prefabs[index].gameObject.GetComponent<PieceInfo>().elevationChange;
     }
